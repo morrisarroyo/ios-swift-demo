32c644e1fa81e656eea177a2e32bc533bd423d70
Morris Arroyo <morrisarroyo@Morriss-Mac.local>
2017-09-15 14:51:30 -0700

    Initial Commit

---
+++ ios-swift-demoTests/ios_swift_demoTests.swift
@@ -0,0 +1,36 @@
+//
+//  ios_swift_demoTests.swift
+//  ios-swift-demoTests
+//
+//  Created by Morris Arroyo on 2017-09-15.
+//  Copyright © 2017 Morris Arroyo. All rights reserved.
+//
+
+import XCTest
+@testable import ios_swift_demo
+
+class ios_swift_demoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
